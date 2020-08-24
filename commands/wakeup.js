706ca6b4abf4b3085cb7a43a57df87de66e484b5
sgarg03 <sat.garg03@gmail.com>
2020-08-24 15:05:39 -0700

    :sparkles:added ping random user and delete message to make anonymous

--- commands/wakeup.js
+++ commands/wakeup.js
@@ -4,19 +4,32 @@ module.exports = {
 	name: 'wakeup',
 	description: 'WakeUp Server!',
 	execute(message, args) {
+		//gets a random users ID
 		function getRandomUser(){
+			//put all members in an array
 			let user = message.guild.members.cache.array();
+			//get a random number
 			let randomUser = Math.floor(Math.random() * user.length)
+			//pick a random member
 			return user[randomUser]
 		}
+
+		//If no args then ping only one random user
 		if (!args.length) {
-			message.channel.send(`${getRandomUser()}`);
+			message.channel.send(`${getRandomUser()}`).then( message => {
+			//delete bot and user message to make it anonymous
+				message.delete({ timeout: 1000 })			
+			})
+			message.delete({ timeout: 1000 });
 			return
 		}
 
-		var i;
-		for (i = 0; i < args[0]; i++) {
-			message.channel.send(`${getRandomUser()}`);
+		//ping random person args[0] amount of times
+		message.delete({ timeout: 1000 });
+		for (let i = 0; i < args[0]; i++) {
+			message.channel.send(`${getRandomUser()}`).then( message => {
+				message.delete()			
+			})
 		}
 	},
 };

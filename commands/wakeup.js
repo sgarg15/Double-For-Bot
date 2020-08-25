133ed6bb01dd0a2bde6c8f9733140e05f74e9e04
sgarg03 <sat.garg03@gmail.com>
2020-08-25 15:56:32 -0700

    :zap:improved wakeup command

--- commands/wakeup.js
+++ commands/wakeup.js
@@ -1,28 +1,41 @@
 const Discord = require('discord.js');
-const client = require('C:/Users/apnas/OneDrive - School District No. 36 (Surrey)/Documents/Double-For-Bot/index.js');
+const client = require('../index.js');
 
 module.exports = {
 	name: 'wakeup',
 	description: 'WakeUp Server!',
 	execute(message, args) {
+		let userToPing = args[0];
+		let numberOfTimes = args[1];
+		let numberOfTimesDone = 0;
+		let pingSystem;
+
 		if(args.length != 2){
 			message.channel.send(`You didn't provide any arguments. Please use the help command to learn more, ${message.author}! `);
 			return;
 		}
 
-		let userToPing = args[0];
-		let numberOfTimes = args[1];
+		function sendPingMessage(){
+			numberOfTimesDone++;
+			message.channel.send(`${userToPing}`).then( message => {
+				message.delete()			
+			})
 
+			if(numberOfTimesDone == numberOfTimes){
+				clearInterval(pingSystem);
+				return;
+			}
+		}
 		if(message.mentions.has(client.users.cache.get("403744585970352139"))){
 			message.channel.send(`You can't ping the creator`);
 			return;
 		}
 		
-		message.delete();
-		for (let i = 0; i < args[1]; i++) {
-			message.channel.send(`${userToPing}`).then( message => {
-				message.delete()			
-			})
+		if(numberOfTimes <= 15){
+			message.delete();
+			pingSystem = setInterval(sendPingMessage, 3000);
+		} else{
+			message.channel.send(`You can't ping more than 15 times`);
 		}
 	},
 };
